d53182a981cf25cf705be652f2ed79c7e37c6a04
John Sierra <sierrajp73@gmail.com>
2024-06-04 17:12:57 -0400

    Added UI to LogIn

--- src/ui/auth/LogIn.tsx
+++ src/ui/auth/LogIn.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function LogIn({
   firstName,
   VID,
@@ -6,12 +8,14 @@ export default function LogIn({
   VID: string;
 }) {
   return (
-    <div>
+    <>
       <h1 className="text-[48px]">Welcome!</h1>
-      <p className="text-[24px]">{firstName},</p>
-      <p className="text-[24px]">
-        you are <span className="text-val-red font-bold">signed out</span>
-      </p>
-    </div>
+      <div>
+        <p className="text-[24px]">{firstName},</p>
+        <p className="text-[24px]">
+          you are <span className="text-val-red font-bold">signed out</span>
+        </p>
+      </div>
+    </>
   );
 }
